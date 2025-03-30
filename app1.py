4ce289faae7225f3ae7521e9316a4068adf2b6e6
s1906-pjwstk <s1906@pjwstk.edu.pl>
2025-03-30 17:45:14 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -22,7 +22,7 @@ def main():
     left, right = st.columns(2)
     prediction = st.container()
 
-    st.image("https://i.kym-cdn.com/entries/icons/mobile/000/026/489/crying.jpg")  # grafika
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/1280px-RMS_Titanic_3.jpg")  # grafika
 
     with overview:
         st.title("Predykcja przeżycia pasażera Titanica")
